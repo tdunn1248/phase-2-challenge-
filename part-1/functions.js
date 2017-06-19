09a0e3494070ff80a6886456a68f72d4a28d7292
rover <tdunn1248@gmail.com>
2017-06-19 12:28:48 -0700

    snippet function

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,13 +1,24 @@
 // Show a weekday
-
 function weekday(date) {
-  const theDay = new Date(date).getDay()
+  const dayNum = new Date(date).getDay()
   const weekday = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  const values = weekday[theDay]
-    return values
+  const dayString = weekday[dayNum]
+    return dayString
 }
 
 let date = new Date(2017, 5, 19)
 weekday(date)
-
 weekday(new Date(2017, 5, 15))
+
+// Get a snippet from text
+function snippet(string, maxlength) {
+  const daString = string.substr(0, maxlength)
+  if(string.length > maxlength) {
+    const ellip = daString.concat('...')
+    return ellip
+  }
+  return daString
+}
+
+let following = snippet('For the following excercises,', 10)
+let helloWorld = snippet('Hello, world!', 20)
